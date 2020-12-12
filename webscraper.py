7721a781850a77d18e9875b552a10f7838e852fd
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-12 13:29:54 -0600

    data sourced tv show page

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,20 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+bachelors = []                                                                      # Declare Variable
+bachelors_imgs = []                                                                 
 
-# Anchor extraction from HTML document
+show_website = requests.get('https://abc.com/shows/the-bachelorette/cast')          # Make a request to the site / get it as a string
 
-show_website = requests.get('https://www.nbc.com/the-voice/credits/artists/season-19')
-soup = BeautifulSoup(show_website.content, 'html.parser')
-artists = soup.findAll("div", {"class": "grid__tiles"})
-print(artists)
+soup = BeautifulSoup(show_website.content, 'html.parser')                           # Pass the string to a BeatifulSoup object
+cast = soup.find_all(class_='tile__name')
+cast_imgs = soup.find_all('picture')
+
+the_bachelorette = cast[0].get_text()                                               # The Bachelorette data
+the_bachelorette_img = cast_imgs[1]
+
+chris_harrison_host = cast[1].get_text()                                            # Chris Harrison Data
+chris_harrison_host_img = cast_imgs[1]
+
+for i in range(2, len(cast, cast_imgs)):                                                       # Create list of the bachelors
+    bachelors.append(cast[i].get_text())
+    bachelors_imgs.append(cast_imgs[i])
